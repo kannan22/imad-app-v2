63c0a56b3ab9c9cd9c53761563fe8bff275943a3
kannan22 <kannana122@yahoo.com>
2017-02-11 17:27:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ var img = document.getElementById('madi');
 var marginleft = 0;
 function moveRight () 
 {
-    marginleft = marginleft + 10;
+    marginleft = marginleft + 5;
     img.style.marginleft = marginleft + 'px';
 }
 img.onclick = function ()
 {
-var interval = setInterval(moveRight, 100);
+var interval = setInterval(moveRight, 50);
 };
