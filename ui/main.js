29807bd7f43d4297c713980ec0dc782a31c43882
kannan22 <kannana122@yahoo.com>
2017-02-11 17:14:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
+
+//change the text of center text
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//move image
+var img = document.getElementById(madi);
+img.onclick = function ()
+{
+  img.style.marginleft = '100px';
+};
